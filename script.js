9991151c096e6a34fd10565bf7a7fc5daff3ec08
Sonu Kumar <61611771+SonuKr95@users.noreply.github.com>
2022-12-02 23:51:49 +0530

    added media queries & javascript

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const menu = document.getElementById("nav-icon");
+const newmenu = document.querySelector(".nav-menu");
+const closedmenu = document.querySelector(".close-menu");
 
+function showmenu() {
+  newmenu.classList.toggle("nonedisplay");
+}
+
+menu.addEventListener("click", showmenu);
+// newmenu.addEventListener("click", showmenu);
+
+closedmenu.addEventListener("click", hidemenu);
+
+function hidemenu() {
+  newmenu.classList.toggle("nonedisplay");
+}
